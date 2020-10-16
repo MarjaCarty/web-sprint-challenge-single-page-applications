b4551966fa5aa1a9852805e10d3ad29b1f7f14f8
MarjaCarty <marja.d.carty@gmail.com>
2020-10-16 11:31:41 -0500

    Add initial states and handlers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PizzaForm from "./PizzaForm";
 import Home from "./Home";
 import { Route, Switch } from "react-router-dom";
+import axios from "axios";
+import * as yup from "yup";
 
 const App = () => {
+  ///initial values///
+  const initialFormValues = {
+    name: "",
+    size: "",
+    sauce: false,
+    cheese: false,
+    meat: false,
+    veggie: false,
+  };
+
+  const initialFormErrors = {
+    name: "",
+    size: "",
+  };
+
+  const initialDisabled = true;
+
+  ///state setters///
+
+  const [order, setOrder] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const postNewOrder = (newOrder) => {
+    //axios call in here
+  };
+
+  const onChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const onSubmit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      toppings: ["sauce", "cheese", "meat", "veggie"].filter((topping) => {
+        return formValues[topping];
+      }),
+    };
+    postNewOrder(newOrder);
+  };
+
   return (
     <>
       <Switch>
         <Route path="/pizza">
-          <PizzaForm />
+          <PizzaForm
+            values={formValues}
+            submit={onSubmit}
+            change={onChange}
+            disabled={disabled}
+            errors={formErrors}
+          />
         </Route>
 
         <Route path="/">
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function PizzaForm() {
+export default function PizzaForm(props) {
+  const { values, submit, change, disabled, errors } = props;
+
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
   return (
     <div>
       <Link to="/">Home</Link>
-      <form>
+      <form onSubmit={onSubmit}>
+        <div className="errorMessages">
+          <div>{errors.name}</div>
+          <div>{errors.size}</div>
+        </div>
+
         <label>
           Name
-          <input type="text" name="name" placeholder="Enter your name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            onChange={onChange}
+          />
         </label>
         <label>
           Choose a size:
-          <select name="size">
+          <select name="size" onChange={onChange}>
             <option value="">---Select size---</option>
             <option value="Small">10"</option>
             <option value="Medium">12"</option>
@@ -22,20 +45,23 @@ export default function PizzaForm() {
         <h4>Toppings</h4>
         <label>
           Tomato Basil Sauce
-          <input type="checkbox" name="sauce" />
+          <input type="checkbox" name="sauce" onChange={onChange} />
         </label>
         <label>
           Mozzarella Cheese
-          <input type="checkbox" name="cheese" />
+          <input type="checkbox" name="cheese" onChange={onChange} />
         </label>
         <label>
-          Pork Pepperoni
-          <input type="checkbox" name="meat" />
+          Pork Sausage
+          <input type="checkbox" name="meat" onChange={onChange} />
         </label>
         <label>
           Spinach
-          <input type="checkbox" name="veggie" />
+          <input type="checkbox" name="veggie" onChange={onChange} />
         </label>
+        <button type="submit" disabled={disabled}>
+          Add To Order
+        </button>
       </form>
     </div>
   );
