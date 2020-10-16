079d8a07c9ddd3118894ea0af659f2f61296bea6
MarjaCarty <marja.d.carty@gmail.com>
2020-10-16 10:27:37 -0500

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div>
+        <div className="header">
+          <h1>Lambda Eats</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <a href="#">Help</a>
+          </nav>
+        </div>
+        <div className="hero">
+          <Link to="/pizza">Pizza?</Link>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/pizza">
+          <PizzaForm />
+        </Route>
+
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
     </>
   );
 };
